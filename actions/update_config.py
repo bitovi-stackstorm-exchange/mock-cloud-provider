500ce35868de0b619ef18358dd57fd04b09368fa
Mick McGrath <mick@bitovi.com>
2022-03-15 14:12:19 -0400

    initial actions

---
+++ actions/update_config.py
@@ -0,0 +1,33 @@
+import sys
+import json
+
+from st2common.runners.base_action import Action
+from st2client.client import Client
+from st2client.models import KeyValuePair
+
+class UpdateConfigAction(Action):
+    def run(self, key_name, server_id, num_servers, action):
+      self.client = Client(base_url='http://localhost')
+
+      servers_object = {}
+
+      # if key_name exists, use it
+      servers_json = self.client.keys.get_by_name(name=key_name)
+      if servers_json:
+        servers_object = json.loads(servers_json.value)
+
+
+      if action == "delete":
+        del servers_object[server_id]
+      else:
+        server_object = servers_object[server_id]
+
+        if server_object:
+          server_object["num_servers"] = num_servers
+        else:
+          servers_object[server_id] = {
+            "numServers": num_servers
+          }
+
+      self.client.keys.update(KeyValuePair(name=key_name, value=json.dumps(servers_object)))
+      return { "servers": servers_object }
